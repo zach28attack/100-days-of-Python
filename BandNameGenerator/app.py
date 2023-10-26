ff2563703494e2d1759701036004869cb18027dc
Zachary Casares <rrumble250@gmail.com>
2023-10-25 20:53:12 -0400

    done

--- BandNameGenerator/app.py
+++ BandNameGenerator/app.py
@@ -1,1 +1,7 @@
-print("hello world")
+print("Welcome to the Band Name Generator!")
+
+city = input("Where were your born?")
+
+pet_name = input("Whats the name of your first/current pet?")
+
+print("Your band name is " + city + " " + pet_name)
